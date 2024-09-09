d628f7c148b5bb891dd3c7cfeac204a14ddb86af
Phuonglinh82 <2151013049linh@ou.edu.vn>
2024-09-09 23:17:52 +0700

    update user management

---
+++ bookticketapp/app/src/main/java/com/example/bookticketapp/activities/UpdateUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookticketapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user);
+    }
+}
---
+++ bookticketapp/app/src/main/java/com/example/bookticketapp/events/UserSelectListener.java
@@ -0,0 +1,2 @@
+package com.example.bookticketapp.events;public interface UserSelectListener {
+}
