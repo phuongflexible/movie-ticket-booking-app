3629c5199cb281ae8578b112d75b2d8104d3b019
nmNDdiem <diem.nmngoc@gmail.com>
2024-09-10 14:53:04 +0700

    update database (add datas)

--- bookticketapp/app/src/main/java/com/example/bookticketapp/utils/DatetimeUtils.java
+++ bookticketapp/app/src/main/java/com/example/bookticketapp/utils/DatetimeUtils.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
 
 public class DatetimeUtils {
     public static final String DATE_FORMAT = "dd/MM/yyyy";
-    public static final String TIME_FORMAT = "HH:mm";
+    public static final String TIME_FORMAT = "H:mm";
     public static final String DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
     public static final String DAY_FORMAT = "E d/M";
 
