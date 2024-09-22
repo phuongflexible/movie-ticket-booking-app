34e7f8ebb64228051cf283f2ccbd6a6a0fc2a3e1
Phuonglinh82 <2151013049linh@ou.edu.vn>
2024-09-22 16:26:02 +0700

    add cinema

---
+++ bookticketapp/app/src/main/java/com/example/bookticketapp/activities/AddCinemaActivity.java
@@ -0,0 +1,126 @@
+package com.example.bookticketapp.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.bookticketapp.R;
+import com.example.bookticketapp.dao.CinemaQuery;
+import com.example.bookticketapp.dao.LocationQuery;
+import com.example.bookticketapp.models.Cinema;
+import com.example.bookticketapp.models.Location;
+import com.example.bookticketapp.utils.ImageUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddCinemaActivity extends AppCompatActivity {
+
+    EditText editAddCinemaName, editAddAddress;
+    ImageView newImageCinema;
+    Spinner spnLocateCinema;
+    Button btnConfirmAddCinema;
+    List<String> listCinemasString = new ArrayList<>();
+    LocationQuery locationQuery;
+    ArrayAdapter arrayAdapter;
+    CinemaQuery cinemaQuery;
+
+    private Uri imageFilePath;
+    private Bitmap imageToStore;
+    private static final int PICK_IMAGE_REQUEST = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cinema);
+
+        editAddAddress = findViewById(R.id.editAddAddress);
+        editAddCinemaName = findViewById(R.id.editAddCinemaName);
+        spnLocateCinema = findViewById(R.id.spnLocateCinema);
+        newImageCinema = findViewById(R.id.newImageCinema);
+        btnConfirmAddCinema = findViewById(R.id.btnConfirmAddCinema);
+
+        locationQuery = new LocationQuery(this);
+        listCinemasString = locationQuery.getLocationNames();
+        arrayAdapter = new ArrayAdapter(this, R.layout.item_spinner, listCinemasString);
+        spnLocateCinema.setAdapter(arrayAdapter);
+        cinemaQuery = new CinemaQuery(this);
+
+        newImageCinema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage(newImageCinema);
+            }
+        });
+
+        btnConfirmAddCinema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editAddCinemaName.getText().toString().trim();
+                String address = editAddAddress.getText().toString().trim();
+                int locationId = (int) spnLocateCinema.getSelectedItemId() + 1;
+                byte[] imageCinema = ImageUtils.bitmapToByteArray(imageToStore);
+
+                if (name.equals("") || address.equals("") || locationId == -1 || imageCinema.equals(null))
+                {
+                    Toast.makeText(AddCinemaActivity.this, "Không thêm được do thiếu thông tin", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Cinema cinema = new Cinema(name, address, imageCinema, locationId);
+                    Boolean insert = cinemaQuery.addCinema(cinema);
+                    if (insert == true)
+                    {
+                        Toast.makeText(AddCinemaActivity.this, "Thêm rạp phim thành công", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(AddCinemaActivity.this, "Thêm rạp phim thất bại", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void chooseImage(View objectView) {
+        try
+        {
+            Intent objectIntent = new Intent();
+            objectIntent.setType("image/*");
+
+            objectIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(objectIntent, PICK_IMAGE_REQUEST);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+                imageFilePath = data.getData();
+                imageToStore = MediaStore.Images.Media.getBitmap(getContentResolver(), imageFilePath);
+
+                newImageCinema.setImageBitmap(imageToStore);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- bookticketapp/app/src/main/java/com/example/bookticketapp/fragments/CinemaAdminFragment.java
+++ bookticketapp/app/src/main/java/com/example/bookticketapp/fragments/CinemaAdminFragment.java
@@ -1,6 +1,7 @@
 package com.example.bookticketapp.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.bookticketapp.R;
+import com.example.bookticketapp.activities.AddCinemaActivity;
 import com.example.bookticketapp.adapters.CinemaListViewAdapter;
 import com.example.bookticketapp.dao.CinemaQuery;
 import com.example.bookticketapp.models.Cinema;
@@ -18,11 +21,6 @@ import com.example.bookticketapp.models.Cinema;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CinemaAdminFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CinemaAdminFragment extends Fragment {
 
     Activity context;
@@ -30,46 +28,7 @@ public class CinemaAdminFragment extends Fragment {
     List<Cinema> listCinemas;
     CinemaListViewAdapter cinemaAdapter;
     ListView lvCinemas;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CinemaAdminFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CinemaAdminFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CinemaAdminFragment newInstance(String param1, String param2) {
-        CinemaAdminFragment fragment = new CinemaAdminFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button btnAddCinema;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,6 +43,16 @@ public class CinemaAdminFragment extends Fragment {
 
         lvCinemas = view.findViewById(R.id.listViewCinema);
         lvCinemas.setAdapter(cinemaAdapter);
+
+        btnAddCinema= view.findViewById(R.id.btnAddCinema);
+
+        btnAddCinema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, AddCinemaActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
